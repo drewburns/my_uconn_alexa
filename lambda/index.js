75e0d696ba1f71f0b1f3bffa909cabc06ece4720
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-08-17 23:21:36 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -18,10 +18,12 @@ const LaunchRequestHandler = {
 const MenuIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MenuIntent';
     },
     handle(handlerInput) {
         const speakOutput = 'The menu!';
+        const location = handlerInput["slots"]["location"]["value"];
+        const meal_time = handlerInput["slots"]["time"]["value"];
         return handlerInput.responseBuilder
             .speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
