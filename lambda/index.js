87a45684076c2c331c532a373155e9033da5b503
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-08-19 01:24:47 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -105,7 +105,7 @@ const MenuIntentHandler = {
                 const responseString = await getMenuResponse(location, meal_time);
                 console.log('response String');
                 console.log(responseString);
-                return handlerInput.responseBuilder.speak(responseString).withShouldEndSession(false).getResponse();
+                return handlerInput.responseBuilder.speak(responseString.slice(0,15)).withShouldEndSession(false).getResponse();
             }catch(error) {
                 return handlerInput.responseBuilder.speak("Error occurred").withShouldEndSession(false).getResponse();
             }
