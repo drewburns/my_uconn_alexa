b81c61ee6cb8276ced70d9ff6791e0c4105b67e9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-08-28 15:25:18 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -45,7 +45,6 @@ var getBusRequest = async function(stop) {
     console.log(url);
     return new Promise((resolve, reject) => {
         var req = http.get(url, (res) => {
-        console.log(res);
         var body = "";
 
         res.on("data", (chunk) => {
